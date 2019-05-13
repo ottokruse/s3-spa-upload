bc5c0a868ff9bf322b3305ac75bf8de83f5d49ce
Otto Kruse <ottokrus@amazon.nl>
2019-05-13 13:14:32 +0200

    prefix, custom cache-control, yargs, updated docs

--- README.md
+++ README.md
@@ -16,11 +16,38 @@ Basic usage:
 
     s3-spa-upload dist-dir my-s3-bucket-name
 
+### Clean-up old files
+
 To also clean up old files, specify '--delete' as the last parameter. This will delete *all* files in the bucket that are not included in the current upload (**do** ensure you really want this):
 
     s3-spa-upload dist-dir my-s3-bucket-name --delete
 
-## Cache-Control settings
+### Custom cache-control mapping
+
+You can provide your desired cache-control mapping in a json file that contains a mapping from glob patterns to cache-control headers:
+
+```javascript
+{
+    "index.html": "no-cache",
+    "*.js": "public,max-age=31536000,immutable"
+}
+```
+
+Suppose your file is called `cache-control.json` then use like this:
+
+    s3-spa-upload dist-dir my-s3-bucket-name --cache-control-mapping cache-control.json
+
+If you don't provide a custom mapping, the default will be used, which should be okay for most SPA's, see below.
+
+### Upload to a prefix
+
+By default the SPA will be uploaded to the root of your S3 bucket. If you don't want this, specify the prefix to use:
+
+    s3-spa-upload dist-dir my-s3-bucket-name --prefix mobile
+
+Note that when used in conjunction with `--delete`, this means only old files matching that same prefix will be deleted.
+
+## Default Cache-Control settings
 
 File/ext | Cache setting | Description
 ---------|---------------|----------
--- index.ts
+++ index.ts
@@ -1,33 +1,50 @@
 #!/usr/bin/env node
 
 import { readdirSync, readFileSync, statSync } from 'fs';
-import { basename, extname, join } from 'path';
+import { extname, join } from 'path';
 import { contentType } from 'mime-types';
 import { S3 } from 'aws-sdk';
+import minimatch from 'minimatch';
+import yargs from 'yargs';
 
 const S3CLIENT = new S3();
 
-interface Mapping {
-    [type: string]: string;
+interface CacheControlMapping {
+    [glob: string]: string;
 }
 
-const CACHE_FOREVER = 'public,max-age=31536000,immutable';
-const CACHE_ONE_DAY = 'public,max-age=86400';
-const NO_CACHE = 'no-cache';
-const DEFAULT_CACHE_CONTROL_MAPPING: Mapping = {
-    css: CACHE_FOREVER,
-    js: CACHE_FOREVER,
-    png: CACHE_ONE_DAY,
-    ico: CACHE_ONE_DAY,
-    txt: CACHE_ONE_DAY,
+interface AwsCredentials {
+    accessKeyId: string
+    secretAccessKey: string
+    sessionToken?: string
+}
+interface Options {
+    delete?: boolean;
+    cacheControlMapping?: CacheControlMapping;
+    awsCredentials?: AwsCredentials;
+    prefix?: string;
 }
 
-function getCacheControl(filepath: string) {
-    const filename = basename(filepath);
-    if (filename === 'index.html') {
-        return NO_CACHE;
+
+export const CACHE_FOREVER = 'public,max-age=31536000,immutable';
+export const CACHE_ONE_DAY = 'public,max-age=86400';
+export const NO_CACHE = 'no-cache';
+
+export const DEFAULT_CACHE_CONTROL_MAPPING: CacheControlMapping = {
+    'index.html': NO_CACHE,
+    '*.css': CACHE_FOREVER,
+    '*.js': CACHE_FOREVER,
+    '*.png': CACHE_ONE_DAY,
+    '*.ico': CACHE_ONE_DAY,
+    '*.txt': CACHE_ONE_DAY,
+}
+
+function getCacheControl(filepath: string, cacheControlMapping: CacheControlMapping) {
+    for (let [glob, cacheControl] of Object.entries(cacheControlMapping)) {
+        if (minimatch(filepath, glob, { matchBase: true })) {
+            return cacheControl;
+        }
     }
-    return DEFAULT_CACHE_CONTROL_MAPPING[extname(filename)];
 }
 
 async function walkDirectory(directoryPath: string, callback: (filePath: string) => any) {
@@ -46,46 +63,86 @@ async function walkDirectory(directoryPath: string, callback: (filePath: string)
     return uploaded;
 }
 
-async function uploadToS3(bucket: string, key: string, filePath: string) {
+async function uploadToS3(bucket: string, key: string, filePath: string, prefix: string, cacheControlMapping: CacheControlMapping) {
     const params = {
         Bucket: bucket,
-        Key: key,
+        Key: `${prefix}${key}`,
         Body: readFileSync(filePath),
-        CacheControl: getCacheControl(filePath),
+        CacheControl: getCacheControl(filePath, cacheControlMapping),
         ContentType: contentType(extname(filePath)) || undefined,
     };
     await S3CLIENT.putObject(params).promise();
-    console.log(`Uploaded s3://${bucket}/${key} from ${filePath}`);
-    return key;
+    console.log(`Uploaded s3://${params.Bucket}/${params.Key} | cache-control=${params.CacheControl} | content-type=${params.ContentType}`);
+    return params.Key;
 }
 
-async function removeOldFiles(bucket: string, uploaded: string[]) {
-    const resp = await S3CLIENT.listObjectsV2({ Bucket: bucket }).promise();
-    const existingFiles = resp.Contents!.map(obj => obj.Key!);
-    const filesToDelete = existingFiles.filter(key => !uploaded.includes(key));
+async function removeOldFiles(bucket: string, uploaded: string[], prefix: string) {
+    let existingFiles: string[] = [];
+    await new Promise((resolve, reject) => S3CLIENT.listObjectsV2({ Bucket: bucket }).eachPage((err, page) => {
+        if (err) {
+            reject(err);
+            return false;
+        }
+        if (page) {
+            existingFiles = existingFiles.concat(page.Contents!.map(obj => obj.Key!));
+            return true;
+        }
+        resolve();
+        return false;
+    }));
+    const filesToDelete = existingFiles
+        .filter(key => key.startsWith(prefix))
+        .filter(key => !uploaded.includes(key));
     await Promise.all(filesToDelete.map(async (key) => {
         await S3CLIENT.deleteObject({ Bucket: bucket, Key: key }).promise()
         console.log(`Deleted old file: ${key}`);
     }));
-    console.log(`Deleted ${filesToDelete.length} old files`);
+    return filesToDelete;
+}
+
+export default async function s3SpaUpload(dir: string, bucket: string, options: Options = {}) {
+    const regexp = new RegExp(`^${dir}/?`);
+    if (!options.cacheControlMapping) {
+        options.cacheControlMapping = DEFAULT_CACHE_CONTROL_MAPPING;
+    }
+    if (options.awsCredentials) {
+        S3CLIENT.config.update({ credentials: options.awsCredentials });
+    }
+    if (!options.prefix) {
+        options.prefix = '';
+    } else {
+        options.prefix = options.prefix.endsWith('/') ? options.prefix : `${options.prefix}/`;
+    }
+    const uploaded = await walkDirectory(dir, (filePath) => uploadToS3(bucket, filePath.replace(regexp, ''), filePath, options.prefix!, options.cacheControlMapping!));
+    console.log(`Uploaded ${uploaded.length} files`);
+    if (options.delete) {
+        const deleted = await removeOldFiles(bucket, uploaded, options.prefix);
+        console.log(`Deleted ${deleted.length} old files`);
+    }
 }
 
 async function main() {
-    const dir = process.argv[2];
-    if (!dir) {
-        console.error('The name of the directory to upload should be provided as first argument');
-        process.exit(1);
+    const args = yargs
+        .command('$0 <directory> <bucketname> [options]', 'Upload a dist/build directory containing a SPA (React, Angular, Vue, ...) to AWS S3')
+        .string('directory')
+        .string('bucketname')
+        .boolean('d')
+        .alias('d', 'delete')
+        .describe('d', 'Delete old files from the S3 bucket')
+        .describe('cache-control-mapping', 'Path to custom JSON file that maps glob patterns to cache-control headers')
+        .string('cache-control-mapping')
+        .alias('p', 'prefix')
+        .describe('p', 'Path prefix to prepend to every S3 object key of uploaded files')
+        .string('prefix')
+        .help()
+        .wrap(88)
+        .argv;
+
+    let cacheControlMapping: CacheControlMapping | undefined;
+    if (args['cache-control-mapping']) {
+        cacheControlMapping = JSON.parse(readFileSync(args['cache-control-mapping']).toString());
     }
-    const bucket = process.argv[3];
-    if (!bucket) {
-        console.error('The S3 bucket name should be provided as second argument');
-        process.exit(1);
-    }
-    const regexp = new RegExp(`^${dir}/?`);
-    const uploaded = await walkDirectory(dir, (filePath) => uploadToS3(bucket, filePath.replace(regexp, ''), filePath));
-    if (process.argv[4] === '--delete') {
-        await removeOldFiles(bucket, uploaded);
-    }
+    await s3SpaUpload(args.directory!, args.bucketname!, { delete: args.delete, cacheControlMapping, prefix: args.prefix });
 }
 
 if (require.main === module) {
