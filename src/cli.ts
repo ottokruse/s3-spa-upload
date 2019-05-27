8b86230c5abfd1b7960577922fd46eda89b35542
Otto Kruse <ottokrus@amazon.nl>
2019-05-27 16:50:56 +0200

    Split off cli code into separate file

---
+++ src/cli.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { readFileSync } from 'fs';
+import s3SpaUpload, { CacheControlMapping } from './index';
+import yargs from 'yargs';
+
+async function main() {
+    const args = yargs
+        .command('$0 <directory> <bucketname> [options]', 'Upload a dist/build directory containing a SPA (React, Angular, Vue, ...) to AWS S3')
+        .string('directory')
+        .string('bucketname')
+        .boolean('d')
+        .alias('d', 'delete')
+        .describe('d', 'Delete old files from the S3 bucket')
+        .describe('cache-control-mapping', 'Path to custom JSON file that maps glob patterns to cache-control headers')
+        .string('cache-control-mapping')
+        .alias('p', 'prefix')
+        .describe('p', 'Path prefix to prepend to every S3 object key of uploaded files')
+        .string('prefix')
+        .help()
+        .wrap(88)
+        .argv;
+
+    let cacheControlMapping: CacheControlMapping | undefined;
+    if (args['cache-control-mapping']) {
+        cacheControlMapping = JSON.parse(readFileSync(args['cache-control-mapping']).toString());
+    }
+    await s3SpaUpload(args.directory!, args.bucketname!, { delete: args.delete, cacheControlMapping, prefix: args.prefix });
+}
+
+if (require.main === module) {
+    main().catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-#!/usr/bin/env node
-
 import { readdirSync, readFileSync, statSync } from 'fs';
 import { extname, join } from 'path';
 import { contentType } from 'mime-types';
 import { S3 } from 'aws-sdk';
 import minimatch from 'minimatch';
-import yargs from 'yargs';
+
 
 const S3CLIENT = new S3();
 
-interface CacheControlMapping {
+export interface CacheControlMapping {
     [glob: string]: string;
 }
 
@@ -118,35 +116,4 @@ export default async function s3SpaUpload(dir: string, bucket: string, options: 
         const deleted = await removeOldFiles(bucket, uploaded, options.prefix);
         console.log(`Deleted ${deleted.length} old files`);
     }
-}
-
-async function main() {
-    const args = yargs
-        .command('$0 <directory> <bucketname> [options]', 'Upload a dist/build directory containing a SPA (React, Angular, Vue, ...) to AWS S3')
-        .string('directory')
-        .string('bucketname')
-        .boolean('d')
-        .alias('d', 'delete')
-        .describe('d', 'Delete old files from the S3 bucket')
-        .describe('cache-control-mapping', 'Path to custom JSON file that maps glob patterns to cache-control headers')
-        .string('cache-control-mapping')
-        .alias('p', 'prefix')
-        .describe('p', 'Path prefix to prepend to every S3 object key of uploaded files')
-        .string('prefix')
-        .help()
-        .wrap(88)
-        .argv;
-
-    let cacheControlMapping: CacheControlMapping | undefined;
-    if (args['cache-control-mapping']) {
-        cacheControlMapping = JSON.parse(readFileSync(args['cache-control-mapping']).toString());
-    }
-    await s3SpaUpload(args.directory!, args.bucketname!, { delete: args.delete, cacheControlMapping, prefix: args.prefix });
-}
-
-if (require.main === module) {
-    main().catch(err => {
-        console.error(err);
-        process.exit(1);
-    });
 }
